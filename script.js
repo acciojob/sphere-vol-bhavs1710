75ff6d750bca4fd630bff26c40447ec682825971
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-05-25 20:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function volume_sphere() {
     //Write your code here
-	let radius=document.getElementbyId("radius");
-	let volume=4/3(3.14*(radius*radius*radius));
+	let radiusnew=document.getElementbyId("radius");
+	let volume=4/3(3.14*(radiusnew*radiusnew*radiusnew));
   return volume;
 } 
-
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
