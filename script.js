b1d11b5fcfa29c8d0f7e2b4ba3de270ffbdc1347
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-05-26 12:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function volume_sphere(e) {
 	e.preventDefault();
 	let r = document.getElementById("radius").value;
 	let vol=(4/3)*(Math.PI)*(r*r*r)
-     document.getElementById("volume").value='${vol}';
+     document.getElementById("volume").value=vol;
 } 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
